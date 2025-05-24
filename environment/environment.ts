767cd4bbdc6aebc38efdaceb2534bc4233fd90bc
ahadnawaz585 <ahadnawaz585@gmail.com>
2025-05-24 19:53:14 +0500

    added environment

--- environment/environment.ts
+++ environment/environment.ts
@@ -1,6 +1,6 @@
 console.log(process.env.NEXT_PUBLIC_API_URL)
 export const environment = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://be-filler-server.onrender.com',
   secretKey: process.env.NEXT_PUBLIC_SECRET_KEY || 'secret',
   // apiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://v21qbr74-3001.euw.devtunnels.ms/'
   // apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://174.136.29.206:3000'
